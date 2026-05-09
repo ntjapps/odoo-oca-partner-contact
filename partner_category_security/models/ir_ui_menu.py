7d36d80218f174e9867c55a1b3c898add131001c
Nathanael Yusuf Tjahjanadi <nathanaelytj@ntj.co.id>
2026-05-09 01:55:50 +0000

    [FIX] partner_category_security: rename groups_id -> group_ids / _get_group_ids for v19
    Odoo 19 renamed res.users.groups_id -> group_ids/all_group_ids and
    ir.ui.menu.groups_id -> group_ids. Update the menu data record and the
    ormcache key on _visible_menu_ids accordingly. The ormcache key now
    mirrors upstream ir.ui.menu (frozenset(self.env.user._get_group_ids())).

--- partner_category_security/models/ir_ui_menu.py
+++ partner_category_security/models/ir_ui_menu.py
@@ -7,7 +7,7 @@ class IrUiMenu(models.Model):
     _inherit = "ir.ui.menu"
 
     @api.model
-    @tools.ormcache("frozenset(self.env.user.groups_id.ids)", "debug")
+    @tools.ormcache("frozenset(self.env.user._get_group_ids())", "debug")
     def _visible_menu_ids(self, debug=False):
         """It is not possible to set !groups in menu items, we do not return the record
         if the user has base.group_system (to avoid the 'duplicate' menu)."""
